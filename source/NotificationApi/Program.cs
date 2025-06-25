6636450dbe5dcd3c2909d9fec745a78b07dbfe1b
marcin <mmkowal@gmail.com>
2025-06-25 10:18:15 +0200

    Improved rabbitMq connection management

--- source/NotificationApi/Program.cs
+++ source/NotificationApi/Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Polly;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
 
 namespace NotificationApi;
 
@@ -9,39 +12,56 @@ public class Program
 
         var builder = WebApplication.CreateBuilder(args);
 
-
         builder.WebHost.ConfigureKestrel(serverOptions =>
         {
-            serverOptions.ListenAnyIP(5671, listenOptions => // Listen for HTTPS on port 5002  
+            serverOptions.ListenAnyIP(5671, listenOptions =>
             {
                 listenOptions.UseConnectionLogging();
             });
-    });
+        });
 
-        //      builder.Services.AddAuthorization();
-        //builder.Services.AddEndpointsApiExplorer();
-        //builder.Services.AddSwaggerGen();
+
+        builder.Services.AddResiliencePipeline("MessagePublishPipeline", static (builder, context) =>
+        {
+            builder. AddRetry(new()
+            {
+                MaxRetryAttempts = 3,
+                BackoffType = DelayBackoffType.Linear,
+                Delay = TimeSpan.FromSeconds(2),
+                ShouldHandle = args => new ValueTask<bool>(args.Outcome.Exception is not null),
+                OnRetry = args =>
+                {
+                    var logger = context.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogWarning($"Retrying message publish. Attempt {args.AttemptNumber}. Exception: {args.Outcome.Exception?.Message}");
+                    return default;
+                }
+            });
+        });
+
+        builder.Services.AddResiliencePipeline("BrokerConnectionPipeline", static (builder, context) =>
+        {
+            builder.AddRetry(new()
+            {
+                MaxRetryAttempts =2,
+                BackoffType = DelayBackoffType.Linear,
+                Delay = TimeSpan.FromSeconds(2),
+                ShouldHandle = args => new ValueTask<bool>(args.Outcome.Exception is BrokerUnreachableException),
+                OnRetry = args =>
+                {
+                    var logger = context.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogWarning($"Retrying connection to broker. Attempt {args.AttemptNumber}. Exception: {args.Outcome.Exception?.Message}");
+                    return default;
+                }
+            });
+        });
 
         builder.Services.AddLogging();
-        builder.Services.AddSingleton<NotificationSender>();
+        builder.Services.AddSingleton<RabbitMqConnectionProvider>();
+        builder.Services.AddTransient<NotificationSender>();
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-        
-
-          //  app.UseSwagger();
-          //  app.UseSwaggerUI();
-        }
-
-        //app.UseHttpsRedirection();
-        //app.UseAuthorization();
-
-
         app.MapGet("/", () => "Running!!");
-
-        app.MapPost("/api/Notifications", async (NotificationSender sender, [FromBody] NotificationRequest request,  CancellationToken cancellationToken) =>
+        app.MapPost("/api/Notifications", async (NotificationSender sender, [FromBody] NotificationRequest request, CancellationToken cancellationToken) =>
             await sender.SendMessageAsync(request, cancellationToken))
             .WithName("SendNotification");
 
---
+++ source/NotificationApi/RabbitMqConnectionProvider.cs
@@ -0,0 +1,95 @@
+﻿using Polly.Registry;
+using RabbitMQ.Client;
+
+namespace NotificationApi
+{
+    public class RabbitMqConnectionProvider : IAsyncDisposable
+    {
+        private Task<IConnection>? _connectionTask;
+
+        private readonly ILogger<RabbitMqConnectionProvider> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly ConnectionFactory _factory;
+        private readonly ResiliencePipelineProvider<string> _pipelineProvider;
+        private readonly SemaphoreSlim _semaphore = new(1, 1);  
+
+        public RabbitMqConnectionProvider(ILogger<RabbitMqConnectionProvider> logger, IConfiguration configuration, ResiliencePipelineProvider<string> pipelineProvider)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _pipelineProvider = pipelineProvider;
+
+            _factory = new ConnectionFactory
+            {
+                HostName = _configuration.GetValue<string>("MessageBroker:address"),
+                Port = _configuration.GetValue<int>("MessageBroker:port"),
+                AutomaticRecoveryEnabled = true,
+                NetworkRecoveryInterval = TimeSpan.FromSeconds(10),
+                ClientProvidedName = "NotificationApiClient",
+            }; 
+        }
+
+        public async ValueTask<IConnection> GetConnectionAsync()
+        {
+            await _semaphore.WaitAsync();
+            try
+            {
+                if (_connectionTask == null || !_connectionTask.Result.IsOpen)
+                {
+                    _connectionTask = CreateConnectionWithRetryAsync();
+                }
+                return await _connectionTask;
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private async Task<IConnection> CreateConnectionWithRetryAsync()
+        {
+            var retryPolicy = _pipelineProvider.GetPipeline("BrokerConnectionPipeline");
+            IConnection? connection = null;
+
+            await retryPolicy.ExecuteAsync(async _ =>
+            {
+                connection = await _factory.CreateConnectionAsync();
+            });
+
+            if (connection == null)
+            {
+                throw new InvalidOperationException("Failed to establish a connection to RabbitMQ.");
+            }
+
+            connection.ConnectionShutdownAsync += (_, e) =>
+            {
+                _logger.LogError($"Connection to RabbitMQ broker {connection.Endpoint.HostName}:{connection.RemotePort} has been shutdown. Reason: {e.ReplyText}");
+                return Task.CompletedTask;
+            };
+
+            connection.CallbackExceptionAsync += (_, e) =>
+            {
+                _logger.LogError($"Callback exception in RabbitMQ connection: {e.Exception.Message}");
+                return Task.CompletedTask;
+            };
+
+            return connection;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_connectionTask != null)
+            {
+                try
+                {
+                    var connection = await _connectionTask;
+                    await connection.DisposeAsync();
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogWarning(ex, "Exception occurred while disposing RabbitMQ connection.");
+                }
+            }
+        }
+    }
+}
